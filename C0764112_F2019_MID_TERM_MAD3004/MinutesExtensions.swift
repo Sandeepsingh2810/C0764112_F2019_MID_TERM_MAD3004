ed0965c432482984247a4e11ea60c20ab6e7be17
sandeepsingh2810 <sandeepjangra0100@gmail.com>
2019-10-19 18:28:10 -0400

    minutes extensions fclass created

---
+++ C0764112_F2019_MID_TERM_MAD3004/MinutesExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  MinutesExtensions.swift
+//  C0764112_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+extension Int
+    
+{
+    
+    func minuteFormat() -> String {
+     
+        return "\(self) Minutes"
+  
+    }
+    
+}
