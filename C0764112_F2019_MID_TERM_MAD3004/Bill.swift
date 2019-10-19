f717cfa0e331ed24f72699845609b25e1b102781
sandeepsingh2810 <sandeepjangra0100@gmail.com>
2019-10-19 17:56:26 -0400

    variable,init and self declared in bill class

--- C0764112_F2019_MID_TERM_MAD3004/Bill.swift
+++ C0764112_F2019_MID_TERM_MAD3004/Bill.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+class Bill : IDisplay
+    
+{
+    
+    func iDisplay()
+    {
+        
+    }
+    var billId : String
+    
+    var billDate :  String
+    
+    enum Types
+    {
+        
+case Internet
+        case Hydro
+        case Mobile
+        
+    }
+    
+    
+    var billType : Types
+    
+    var totalBillAmount : Float = 0
+
+    
+    init(billId : String , billDate : String, billType : Types)
+    {
+       
+        self.billId = billId
+        
+        self.billDate = billDate
+        self.billType = billType
+        
+    }
+   
+}
